a28f26b0041a1abde38ee6c5a49d98e8af82c001
NahinAhmed28 <nahin.ahmed28@gmail.com>
2022-03-06 01:33:59 +0600

    commentseeder

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 use Faker;
 class CommentSeeder extends Seeder
@@ -13,15 +14,13 @@ class CommentSeeder extends Seeder
      */
     public function run()
     {
-
         $faker = Faker\Factory::create();
 
         for ($i = 0; $i < 10; $i++) {
             \App\Models\Comment::create([
-                'post_id' => $$faker->randomNumber(3),
+                'post_id' => Post::pluck('id')->random(),
                 'title' => $faker->realText(30),
                 'description' => $faker->realText(200),
-                'file_path' => 'abs',
                 'status' => '1'
             ]);
         }
