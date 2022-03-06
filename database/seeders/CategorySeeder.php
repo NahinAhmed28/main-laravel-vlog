f4d15c1ec181b2ecfab070077df507ede430528e
NahinAhmed28 <Nahin.ahmed28@gmail.com>
2022-03-06 16:26:32 +0600

    wip

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -17,12 +17,12 @@ class CategorySeeder extends Seeder
 
 
         $faker = Faker\Factory::create();
-
+        $filePath = public_path('uploads/categoryFiles');
         for ($i = 0; $i < 10; $i++) {
             \App\Models\Category::create([
                 'title' => $faker->realText(30),
                 'description' => $faker->realText(200),
-                'file_path' =>'abs',
+                'file_path' =>$faker->image($filePath,640,480, null, false),
                 'status' => '1'
 
             ]);
